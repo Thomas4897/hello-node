cb9d480f84737369fd1db633608f529012bb4b6d
Thomas4897 <t.maynard4897@gmail.com>
2021-09-20 23:59:08 +0800

    Adding print text

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log("hello world");
+
+console.log("");
+
+console.log(
+	"HH   HH        lll lll                                  lll      dd !!!"
+);
+console.log(
+	"HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd !!!"
+);
+console.log(
+	"HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!!"
+);
+console.log(
+	"HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd"
+);
+console.log(
+	"HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!!"
+);
+
+console.log("");
+
+console.log(
+	"TTTTTT h                       M   M                          d !!!"
+);
+console.log(
+	"  TT   h                       MM MM                          d ! !"
+);
+console.log(
+	"  TT   hhh  ooo mmmm   aa  ss  M M M  aa y  y nnn   aa rrr  ddd ! !"
+);
+console.log(
+	"  TT   h  h o o m m m a a  s   M   M a a y  y n  n a a r   d  d  !"
+);
+console.log(
+	"  TT   h  h ooo m m m aaa ss   M   M aaa  yyy n  n aaa r    ddd !!!"
+);
+console.log("                                            y                  ");
+console.log("                                         yyy                   ");
